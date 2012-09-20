b4f623ea031f02a39618e6a252adf3cca7b60008
Timothee Groleau <timothee.groleau@mig33global.com>
2012-09-21 00:55:48 +0800

    adding support for (some) named colors, hex strings, plain numbers, and objects

--- color.js
+++ color.js
@@ -1,26 +1,139 @@
-var Color = function(r, g, b, a)
+(function(undefined)
 {
-	this.r = Math.max(0, Math.min(r, 255));
-	this.g = Math.max(0, Math.min(g, 255));
-	this.b = Math.max(0, Math.min(b, 255));
-	this.a = isNaN(a) ? 1 : Math.max(0, Math.min(a, 1));
-}
+	Color = function(r, g, b, a)
+	{
+		this.r = Math.max(0, Math.min(r, 255));
+		this.g = Math.max(0, Math.min(g, 255));
+		this.b = Math.max(0, Math.min(b, 255));
+		this.a = isNaN(a) ? 1 : Math.max(0, Math.min(a, 1));
+	}
+
+	Color.create = function(entry)
+	{
+		if (entry instanceof Color) return entry;
+
+		var ztype = typeof(entry);
+		if (ztype == "number")
+		{
+			entry = Math.round(entry);
+			return new Color(
+				  (entry & 0xff0000) >> 16
+				, (entry & 0xff00) >> 8
+				, (entry & 0xff)
+			);
+		}
+		else if (ztype == "string")
+		{
+			entry = entry.toLowerCase();
+			if (named_cache[entry]) return named_cache[entry];
+			if (named[entry]) return named_cache[entry] = this.parseHexString(named[entry]);
+			return this.parseHexString(entry);
+		}
+		else if (ztype == "object")
+		{
+			if (!(isNaN(entry.r) || isNaN(entry.g) || isNaN(entry.b)))
+			{
+				return new Color(entry.r, entry.g, entry.b, entry.a);
+			} 
+		}
+
+		throw "Not a color";
+	}
+
+	
+	// regexps will only be lazy-initialized
+	var res = null;
+	Color.parseHexString = function(s)
+	{
+		if (!res)
+		{
+			res = [
+				  [new RegExp('^#([0-9a-f])$'), function(m)
+				  {
+					var v = parseInt(m[1] + m[1], 16);
+					return new Color(v, v, v);
+				  }]
+				, [new RegExp('^#([0-9a-f]{2})$'), function(m)
+				  {
+					var v = parseInt(m[1], 16); 
+					return new Color(v, v, v);
+				  }]
+				, [new RegExp('^#([0-9a-f]{3})$'), function(m)
+				  {
+					var s = m[1]; 
+					return new Color(
+						  parseInt(s.charAt(0) + s.charAt(0), 16)
+						, parseInt(s.charAt(1) + s.charAt(1), 16) 
+						, parseInt(s.charAt(2) + s.charAt(2), 16)
+					);
+				  }]
+				, [new RegExp('^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$'), function(m)
+				  {
+					return new Color(
+						  parseInt(m[1], 16)
+						, parseInt(m[2], 16)
+						, parseInt(m[3], 16)
+					);
+				  }]
+			];
+		}
+
+		var m;
+		for (idx=res.length; idx-->0;)
+		{
+			if (m = res[idx][0].exec(s)) return res[idx][1](m);
+		}
+
+		throw "Not a valid hex color";
+	}
 
 
-Color.prototype.toRGBAString = function()
-{
-	var channels = [this.r, this.g, this.b];
-	return  (this.a >= 1 ? 'rgb(' : (channels.push(this.a), 'rbga(')) + channels.join(', ') + ')';
-}
+	var p = Color.prototype;
+	p.toRGBAString = function()
+	{
+		var channels = [this.r, this.g, this.b];
+		return  (this.a >= 1 ? 'rgb(' : (channels.push(this.a), 'rbga(')) + channels.join(', ') + ')';
+	}
 
-Color.prototype.getMidColor = function(targetCol, ratio)
-{
-	ratio = isNaN(ratio) ? .5 : Math.max(0, Math.min(ratio, 1));
-	return new Color(
-		  this.r + (Math.round((targetCol.r - this.r) * ratio))
-		, this.g + (Math.round((targetCol.g - this.g) * ratio))
-		, this.b + (Math.round((targetCol.b - this.b) * ratio))
-		, this.a + (Math.round((targetCol.a - this.a) * ratio))
-	);
-}
+	p.toHexString = function()
+	{
+		var r = this.r.toString(16);
+		var g = this.g.toString(16);
+		var b = this.b.toString(16);
+		return '#'
+			+ (r.length < 2 ? '0' : '' ) + r
+			+ (g.length < 2 ? '0' : '' ) + g
+			+ (b.length < 2 ? '0' : '' ) + b;
+	}
 
+	p.getMidColor = function(targetCol, ratio)
+	{
+		ratio = isNaN(ratio) ? .5 : Math.max(0, Math.min(ratio, 1));
+		return new Color(
+			  this.r + (Math.round((targetCol.r - this.r) * ratio))
+			, this.g + (Math.round((targetCol.g - this.g) * ratio))
+			, this.b + (Math.round((targetCol.b - this.b) * ratio))
+			, this.a + (Math.round((targetCol.a - this.a) * ratio))
+		);
+	}
+
+	var named_cache = {}, named = {
+		     aqua: "#00ffff"
+		,   black: "#000000"
+		,    blue: "#0000ff"
+		, fuchsia: "#ff00ff"
+		,    gray: "#808080"
+		,   green: "#008000"
+		,    lime: "#00ff00"
+		,  maroon: "#800000"
+		,    navy: "#000080"
+		,   olive: "#808000"
+		,  purple: "#800080"
+		,     red: "#ff0000"
+		,  silver: "#c0c0c0"
+		,    teal: "#008080"
+		,   white: "#ffffff"
+		,  yellow: "#ffff00"
+	};
+	
+})();
--- gradient.js
+++ gradient.js
@@ -1,61 +1,65 @@
-Gradient = function(/*variable length Color objects*/)
+(function(undefined)
 {
-	var len = arguments.length;
-	if (len <= 1 ) throw "A Gradient needs at least 2 colors";
-
-	// constructor assume equidistant colors
-	var step = 1 / (len-1);
-	var cur = 0;
-	var m = this.markers = [];
-	for(var idx=0; idx<len; idx++, cur+=step)
+	Gradient = function(/*variable length Color objects*/)
 	{
-		m.push({val: cur, col: arguments[idx]});
+		var len = arguments.length;
+		if (len <= 1 ) throw "A Gradient needs at least 2 colors";
+
+		// constructor assume equidistant colors
+		var step = 1 / (len-1);
+		var cur = 0;
+		var m = this.markers = [];
+		for(var idx=0; idx<len; idx++, cur+=step)
+		{
+			m.push({val: cur, col: Color.create(arguments[idx])});
+		}
+
+		// to prevent potential float errors, we set last entry explicitely as 1;
+		m[len-1].val = 1;
 	}
 
-	// to prevent potential float errors, we set last entry explicitely as 1;
-	m[len-1].val = 1;
-}
-
-Gradient.prototype.addColor = function(val, col)
-{
-	// add a color, garanteeing the markers array is ordered by value
-	var m = this.markers;
-	val = Math.max(0, Math.min(val, 1));
-	for (var idx=m.length; idx-->0;)
+	var p = Gradient.prototype;
+	p.addColor = function(val, col)
 	{
-		if (val === m[idx].val)
+		// add a color, garanteeing the markers array is ordered by value
+		var m = this.markers;
+		val = Math.max(0, Math.min(val, 1));
+		col = Color.create(col);
+		for (var idx=m.length; idx-->0;)
 		{
-			m[idx].col = col;
-			break;
-		}
-		else if (val > m[idx].val)
-		{
-			m.splice(idx + 1, 0, {val: val, col: col});
-			break;
-		}
-	};
-}
+			if (val === m[idx].val)
+			{
+				m[idx].col = col;
+				break;
+			}
+			else if (val > m[idx].val)
+			{
+				m.splice(idx + 1, 0, {val: val, col: col});
+				break;
+			}
+		};
+	}
 
-Gradient.prototype.getColorAt = function(ratio)
-{
-	// 1. find slice where ratio falls
-	var m = this.markers;
-	val = Math.max(0, Math.min(ratio, 1));
-	for (var idx=m.length; idx-->0;)
+	p.getColorAt = function(ratio)
 	{
-		if (ratio === m[idx].val)
+		// 1. find slice where ratio falls
+		var m = this.markers;
+		val = Math.max(0, Math.min(ratio, 1));
+		for (var idx=m.length; idx-->0;)
 		{
-			return m[idx].col;
-		}
-		if (ratio > m[idx].val)
-		{
-			// 2. normalize ratio within slice
-			var slice_ratio = (ratio - m[idx].val) / (m[idx+1].val - m[idx].val);
-			return m[idx].col.getMidColor(m[idx+1].col, slice_ratio);
-		}
-	};
+			if (ratio === m[idx].val)
+			{
+				return m[idx].col;
+			}
+			if (ratio > m[idx].val)
+			{
+				// 2. normalize ratio within slice
+				var slice_ratio = (ratio - m[idx].val) / (m[idx+1].val - m[idx].val);
+				return m[idx].col.getMidColor(m[idx+1].col, slice_ratio);
+			}
+		};
 
-	// should never reach here!
-	throw "Something's fucked up!";
-}
-
+		// should never reach here!
+		throw "Something's fucked up!";
+	}
+})();
